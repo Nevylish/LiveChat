043e01554c730fa5da6416c4bbef3c78c545e32a
Nevylish <bonjour@nevylish.fr>
2026-02-14 21:44:46 +0100

    Add msToFormattedString function

--- src/core/utils/Functions.ts
+++ src/core/utils/Functions.ts
@@ -97,4 +97,29 @@ export namespace Functions {
         }
         return { display: display, param: param };
     };
+
+    export const msToFormattedString = (ms: number) => {
+        const seconds = Math.floor((ms / 1000) % 60);
+        const minutes = Math.floor((ms / (1000 * 60)) % 60);
+        const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+
+        let res = '';
+        if (hours > 0) {
+            if (minutes === 0 && seconds === 0) {
+                return `${hours < 10 ? '0' : ''}${hours}h00m00s`;
+            } else {
+                res += `${hours < 10 ? '0' : ''}${hours}h`;
+            }
+        }
+
+        if (minutes > 0) {
+            if (seconds === 0) {
+                return `${res}${minutes < 10 ? '0' : ''}${minutes}m00s`;
+            } else {
+                res += `${minutes < 10 ? '0' : ''}${minutes}m`;
+            }
+        }
+
+        return `${res}${seconds < 10 ? '0' : ''}${seconds}s`;
+    };
 }
